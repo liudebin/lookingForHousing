d79995b006a06905ee95bc3fdb368f45a9a9d6c8
liuguobin <guobin.liu@holaverse.com>
2018-11-21 17:39:37 +0800

    first blood

---
+++ src/main/java/qian/ling/yi/lookForHousing/LookForHousingApplication.java
@@ -0,0 +1,12 @@
+package qian.ling.yi.lookForHousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookForHousingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookForHousingApplication.class, args);
+	}
+}
